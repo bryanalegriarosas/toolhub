118e0154c6cb303cb7acd84b815d8b54f2713e10
Bryan Alegria <bryanalegriarosas@gmail.com>
2026-03-12 02:47:44 -0600

    Configurando Sitemap.xml

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -29,17 +29,19 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        $sitemap = Sitemap::create();
+        $sitemap = Sitemap::create(config('app.url'));
 
         // Homepage
         $sitemap->add(
             Url::create('/')
+                ->setUrl(config('app.url'))
         );
 
         // Tools
         Tool::all()->each(function ($tool) use ($sitemap) {
             $sitemap->add(
                 Url::create("/tools/{$tool->slug}")
+                    ->setUrl(config('app.url') . "/tools/{$tool->slug}")
             );
         });
 
@@ -47,11 +49,32 @@ class GenerateSitemap extends Command
         Category::all()->each(function ($category) use ($sitemap) {
             $sitemap->add(
                 Url::create("/category/{$category->slug}")
+                    ->setUrl(config('app.url') . "/category/{$category->slug}")
             );
         });
 
+        // Additional pages (About, Contact, Privacy, Terms, Cookie)
+        $additionalPages = [
+            '/about',
+            '/contact', 
+            '/privacy-policy',
+            '/terms-of-service',
+            '/cookie-policy',
+            '/developer-tools',
+            '/security-tools',
+            '/encoding-tools',
+            '/tools'
+        ];
+
+        foreach ($additionalPages as $page) {
+            $sitemap->add(
+                Url::create($page)
+                    ->setUrl(config('app.url') . $page)
+            );
+        }
+
         $sitemap->writeToFile(public_path('sitemap.xml'));
 
-        $this->info('Sitemap generated successfully.');
+        $this->info('Sitemap generated successfully with URL: ' . config('app.url'));
     }
 }
